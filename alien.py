15b19f87d7bcdd1d1f3a25df81d8368940d24ca2
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2025-10-14 16:34:44 +0530

    added the alien and next step is to refactor

--- alien.py
+++ alien.py
@@ -1,19 +1,24 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Alien:
-    """Class to manage the alien"""
+class Alien(Sprite):
+    """Class to manage the single alien of a fleet"""
     def __init__(self,ai_game):
-        """initialize the alien ans set its start posn"""
+        """initalize the alien and set its starting positiomn."""
+        super().__init__()
         self.screen=ai_game.screen
-        self.screen_rect=ai_game.screen.get_rect()
         
-        #load the alien and get recr()
+        #load the alien image and set its rect attribute.
         self.image=pygame.image.load('images/alien.bmp')
         self.rect=self.image.get_rect()
         
-        self.rect.midtop=self.screen_rect.midtop
-    def blitme(self):
-        self.screen.blit(self.image,self.rect)
+        #start each new alien near the top left of the screen.
         
+        self.rect.x=self.rect.width
+        self.rect.y=self.rect.height
+        
+        # store the alien exact horixontal posn.
+        self.x=float(self.rect.x)
+    
         
     
--- alien_invasion.py
+++ alien_invasion.py
@@ -24,7 +24,9 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         self.ship=Ship(self)
         self.bullets=pygame.sprite.Group()
-        self.alien=Alien(self)
+        self.aliens=pygame.sprite.Group()
+        
+        self._create_fleet()
         
     def run_game(self):
         """start the main loop for the game"""
@@ -94,9 +96,25 @@ class AlienInvasion:
         for bullet in self.bullets.sprites():
             bullet.draw_bullet()            
         self.ship.blitme()
-        self.alien.blitme()
+        self.aliens.draw(self.screen)
         
         pygame.display.flip()
+    
+    def _create_fleet(self):
+        """cretae the fleet of aliens."""
+        #creating an alien until there is no room left
+        #spacing b/w alien is one alien width
+        alien=Alien(self)
+        alien_width=alien.rect.width
+        
+        current_x=alien_width
+        
+        while current_x <(self.settings.screen_width -2*alien_width):
+            new_alien=Alien(self)
+            new_alien.x=current_x
+            new_alien.rect.x=current_x
+            self.aliens.add(new_alien)
+            current_x+=2*alien_width
 if __name__=='__main__':
     #make a game instance , and run the game.
     ai=AlienInvasion()
