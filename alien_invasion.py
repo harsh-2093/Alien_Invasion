a7acc37feec0b3e658ca957cb523800c0069ea6b
harsh tripathi <2k24.cs1i.2411233@gmail.com>
2025-10-16 15:37:44 +0530

    fleet of aliens has been added

--- alien_invasion.py
+++ alien_invasion.py
@@ -106,15 +106,24 @@ class AlienInvasion:
         #spacing b/w alien is one alien width
         alien=Alien(self)
         alien_width=alien.rect.width
+        alien_height=alien.rect.height
+        
         
         current_x=alien_width
-        
-        while current_x <(self.settings.screen_width -2*alien_width):
-            new_alien=Alien(self)
-            new_alien.x=current_x
-            new_alien.rect.x=current_x
-            self.aliens.add(new_alien)
-            current_x+=2*alien_width
+        current_y=alien_height
+        while current_y<(self.settings.screen_height - 3*alien_height):
+            while current_x <(self.settings.screen_width -2*alien_width):
+                self._create_alien(current_x,current_y)
+                current_x+=2*alien_width
+            current_x=alien_width
+            current_y+=2*alien_height
+    def _create_alien(self,x_position,y_position):
+        """cretae an alien and place it in thr row"""
+        new_alien=Alien(self)
+        new_alien.x=x_position
+        new_alien.rect.x=x_position
+        new_alien.rect.y=y_position
+        self.aliens.add(new_alien)
 if __name__=='__main__':
     #make a game instance , and run the game.
     ai=AlienInvasion()
